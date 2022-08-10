5ba85fc56fe16639f933ff6004ec45bbcdf80cba
ehlzi <68719266+ehlzi@users.noreply.github.com>
2022-08-10 00:49:54 -0500

    Fixes

--- JAVA - Border Layout Manager/src/Main.java
+++ JAVA - Border Layout Manager/src/Main.java
@@ -26,7 +26,6 @@ public class Main {
         JPanel panel3 = new JPanel();
         JPanel panel4 = new JPanel();
         JPanel panel5 = new JPanel();
-        JPanel panel6 = new JPanel();
 
         panel1.setBackground(Color.red);
         panel2.setBackground(Color.green);
